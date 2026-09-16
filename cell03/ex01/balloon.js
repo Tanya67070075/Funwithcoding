f8d833605040e93153d2524299402678345622cb
Tanya67070075 <67070075@kmitl.ac.th>
2026-09-16 23:04:54 +0700

    Add files via upload
    sec

---
+++ cell03/ex01/balloon.js
@@ -0,0 +1,32 @@
+const balloon = document.getElementById('balloon');
+const colors = ['red', 'green', 'blue'];
+
+let currentSize = 200;
+let colorIndex = 0;
+
+function updateBalloon() {
+    balloon.style.width = currentSize + 'px';
+    balloon.style.height = currentSize + 'px';
+    balloon.style.backgroundColor = colors[colorIndex];
+}
+
+balloon.addEventListener('click', function() {
+    currentSize += 10;
+    
+    if (currentSize > 420) {
+        currentSize = 200;
+        colorIndex = 0;
+    } else {
+        colorIndex = (colorIndex + 1) % colors.length;
+    }
+    
+    updateBalloon();
+});
+
+balloon.addEventListener('mouseleave', function() {
+    if (currentSize > 200) {
+        currentSize -= 5;
+        colorIndex = (colorIndex - 1 + colors.length) % colors.length;
+        updateBalloon();
+    }
+});
